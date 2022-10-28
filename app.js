9b728c9321265d63f3e5f125ddb6eec7a2335bad
agboola fuad <fagboola09@gmail.com>
2022-10-28 09:01:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    if (req.method == 'GET') {
+        res.statusCode = 200;
+        res.write(JSON.stringify({
+            slackUsername: 'whowasi',
+            backend: true,
+            age: 22,
+            bio: 'i don\'t have a bio to write for now'
+        }));
+    }
+    else {
+        res.statusCode = 404;
+        res.write(
+            JSON.stringify({ error: 'please use the get endpoint' })
+        );
+    }
+    res.end();
+});
+
+
+server.listen(3000);
