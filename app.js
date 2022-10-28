e5171020275eb3396a2fd6830a1aac7c13fac5ba
agboola fuad <fagboola09@gmail.com>
2022-10-28 14:06:49 +0100

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Content-Type", "application/json");
     if (req.method == 'GET') {
         res.statusCode = 200;
